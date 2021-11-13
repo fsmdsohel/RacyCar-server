2630c178db651df1f7d827fb30a0f5fbbe9a4925
fsmdsohel <fsmdshohelrana@gmail.com>
2021-11-13 09:09:24 +0600

    env change

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 5000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f9slz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 
-const serviceAccount = require("./racycar-firebase-adminsdk.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
